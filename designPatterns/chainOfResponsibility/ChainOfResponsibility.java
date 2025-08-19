07c02eb138eeded70d3b4b6f1c4a4ce9d32dd2c2
Abhinav Ratna <abhinavratna91@gmail.com>
2025-08-20 00:11:19 +0530

    added code

--- designPatterns/chainOfResponsibility/ChainOfResponsibility.java
+++ designPatterns/chainOfResponsibility/ChainOfResponsibility.java
@@ -1,7 +1,62 @@
 package designPatterns.chainOfResponsibility;
 
+abstract class OrderHandler {
+    protected OrderHandler nextHandler;
+
+    abstract void processOrder(String order);
+
+    void setNextHandler(OrderHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+}
+
+class OrderValidationHandler extends OrderHandler {
+
+    @Override
+    public void processOrder(String order) {
+        System.out.println(order + " order is placed :)");
+        nextHandler.processOrder(order);
+    }
+
+}
+
+class PaymentProcessHandler extends OrderHandler {
+
+    @Override
+    void processOrder(String order) {
+        System.out.println("Payment is getting processed ....");
+        if (nextHandler != null) nextHandler.processOrder(order);
+    }
+}
+
+class FoodPreparationHandler extends OrderHandler {
+    @Override
+    void processOrder(String order) {
+        System.out.println(order + " is getting prepared ....");
+        if (nextHandler != null) nextHandler.processOrder(order);
+    }
+}
+
+class DeliveryConfirmationHandler extends OrderHandler {
+    @Override
+    void processOrder(String order) {
+        System.out.println(order + " is delivered :)");
+        if (nextHandler != null) nextHandler.processOrder(order);
+    }
+}
+
 public class ChainOfResponsibility {
     public static void main(String[] args) {
 
+        OrderValidationHandler orderValidationHandler = new OrderValidationHandler();
+        PaymentProcessHandler paymentProcessHandler = new PaymentProcessHandler();
+        FoodPreparationHandler foodPreparationHandler = new FoodPreparationHandler();
+        DeliveryConfirmationHandler deliveryConfirmationHandler = new DeliveryConfirmationHandler();
+
+        orderValidationHandler.setNextHandler(paymentProcessHandler);
+        paymentProcessHandler.setNextHandler(foodPreparationHandler);
+        foodPreparationHandler.setNextHandler(deliveryConfirmationHandler);
+
+        orderValidationHandler.processOrder("Pizza & Chicken");
     }
 }
